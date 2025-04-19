7928c3733f30fdb5998245a920d45f2af82ceb57
Danila <braun.kc@yandex.ru>
2025-04-19 15:17:00 +0300

    added handler get-all-tasks with redis

--- db-service/internal/cache/redis/redis.go
+++ db-service/internal/cache/redis/redis.go
@@ -2,8 +2,10 @@ package redis
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/BraunKc/todo/db-service/config"
+	"github.com/BraunKc/todo/db-service/internal/repository/models"
 	"github.com/go-redis/redis"
 	"github.com/joho/godotenv"
 	"go.uber.org/zap"
@@ -22,6 +24,20 @@ func InitRedis() *redis.Client {
 	_, err := client.Ping().Result()
 	if err != nil {
 		config.Logger.Fatal("redis init error", zap.Error(err))
+	}
+
+	var allTasks []models.Task
+	config.DB.Model(&models.Task{}).Find(&allTasks)
+
+	for _, task := range allTasks {
+		err := client.HMSet(strconv.FormatUint(uint64(task.ID), 10), map[string]interface{}{
+			"name":        task.Name,
+			"description": task.Description,
+			"complete":    task.Complete,
+		}).Err()
+		if err != nil {
+			config.Logger.Error("add task in redis error", zap.Error(err))
+		}
 	}
 
 	config.Logger.Info("redis inited", zap.Any("redis", client))
--- db-service/internal/http/handlers.go
+++ db-service/internal/http/handlers.go
@@ -1,9 +1,60 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/BraunKc/todo/db-service/config"
+	"github.com/BraunKc/todo/db-service/internal/repository/models"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
 
 func createTask(c *gin.Context) {
+	createTask := struct {
+		Name        string `json:"name"`
+		Description string `json:"description"`
+	}{}
+	if err := c.ShouldBindJSON(&createTask); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": err,
+		})
+		return
+	}
 
+	if createTask.Name == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": "name must be not empty",
+		})
+		return
+	}
+
+	newTask := models.Task{
+		Name:        createTask.Name,
+		Description: createTask.Description,
+		Complete:    false,
+	}
+
+	task := config.DB.First(&models.Task{}, "name =?", newTask.Name)
+	if task.RowsAffected != 0 {
+		c.JSON(http.StatusConflict, gin.H{
+			"err": "task with same name already created",
+		})
+		return
+	}
+
+	result := config.DB.Create(&newTask)
+	if result.Error != nil {
+		config.Logger.Error("add task to DB error", zap.Error(result.Error))
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"err": result.Error,
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"status": "ok",
+	})
 }
 
 func setStatusComplete(c *gin.Context) {
@@ -11,7 +62,74 @@ func setStatusComplete(c *gin.Context) {
 }
 
 func getAllTasks(c *gin.Context) {
+	type Task struct {
+		ID          uint   `json:"id"`
+		Name        string `json:"name"`
+		Description string `json:"description"`
+	}
+	var completed, uncompleted []Task
+	var allTasks []models.Task
 
+	keys, err := config.RedisClient.Keys("*").Result()
+	if err != nil {
+		config.Logger.Error("get redis keys error", zap.Error(err))
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"err": err,
+		})
+	}
+
+	if len(keys) != 0 {
+		for _, key := range keys {
+			task, err := config.RedisClient.HMGet(key, "name", "description", "complete").Result()
+			if err != nil {
+				config.Logger.Error("get task from redis error", zap.Error(err))
+				continue
+			}
+
+			id, err := strconv.ParseUint(key, 10, 64)
+			if err != nil {
+				config.Logger.Error("ParseUint error", zap.Error(err))
+				continue
+			}
+
+			if task[2] == "1" {
+				completed = append(completed, Task{
+					ID:          uint(id),
+					Name:        task[0].(string),
+					Description: task[1].(string),
+				})
+			} else {
+				uncompleted = append(uncompleted, Task{
+					ID:          uint(id),
+					Name:        task[0].(string),
+					Description: task[1].(string),
+				})
+			}
+		}
+	} else {
+		config.DB.Model(&models.Task{}).Find(&allTasks)
+
+		for _, task := range allTasks {
+			if task.Complete {
+				completed = append(completed, Task{
+					ID:          task.ID,
+					Name:        task.Name,
+					Description: task.Description,
+				})
+			} else {
+				uncompleted = append(uncompleted, Task{
+					ID:          task.ID,
+					Name:        task.Name,
+					Description: task.Description,
+				})
+			}
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"uncompleted": uncompleted,
+		"completed":   completed,
+	})
 }
 
 func getTask(c *gin.Context) {
